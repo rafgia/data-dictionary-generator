c95e60f0a06fed27ad5b7e967a35c9ac73de4294
Raffaele Giancotti <rafgiancotti@gmail.com>
2025-03-03 18:31:58 -0500

    Fix: Solved issue #4 code now runnable from command line

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ setup(
     name="data_dictionary_generator",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "click",
-        "pandas"
-    ],
+    install_requires=["click", "pandas"],
     entry_points={
-        'console_scripts': [
-            'generate-dictionary=data_dictionary_generator.cli:generate_dictionary',
+        "console_scripts": [
+            "generate-dictionary=data_dictionary_generator.cli:generate_dictionary",
         ],
     },
     author="Raffaele Giancotti",
     description="A package to generate data dictionaries using Ollama.",
-    url="https://github.com/yourusername/data_dictionary_generator",
+    url="https://github.com/rafgia/data-dictionary-generator",
 )
