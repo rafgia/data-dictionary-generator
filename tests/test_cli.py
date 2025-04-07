749404f1e109d3cd50c06c6fba6de4e60b70284a
Raffaele Giancotti <rafgiancotti@gmail.com>
2025-04-07 13:36:58 -0400

    Improve relationship detection and output generation. Modifie #16

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,5 @@
 from click.testing import CliRunner
-from data_dictionary_generator.cli import generate_dictionary
+from data_dictionary_generator.cli import main
 
 
 def test_generate_dictionary_cli():
@@ -7,9 +7,7 @@ def test_generate_dictionary_cli():
     Test the CLI command for generating the dictionary.
     """
     runner = CliRunner()
-    result = runner.invoke(
-        generate_dictionary, ["tests/test_data", "test_dataset", "output.csv"]
-    )
+    result = runner.invoke(main, ["tests/test_data", "test_dataset", "output.csv"])
     print(result.output)
     assert result.exit_code == 0
     assert "Metadata generation complete" in result.output
